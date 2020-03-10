0cfab0724d6712083d0b68dd637bcdcb8c433336
maikytech <maikytech1307@gmail.com>
2020-03-10 18:41:56 -0500

    Start Project

---
+++ Player.h
@@ -0,0 +1,11 @@
+//  Player.h
+//  Game developed in C++ using the command line.
+//
+//  Created by Maiqui Cedeño on 10/03/20.
+//  Copyright © 2020 PosetoStudio. All rights reserved.
+
+class Player
+{
+    
+};
+
