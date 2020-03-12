97ba6a99ee3dd5bbfb93b125afac76a544f3661a
maikytech <maikytech1307@gmail.com>
2020-03-11 19:13:09 -0500

    Player movement settings

--- Player.cpp
+++ Player.cpp
@@ -7,7 +7,47 @@
 #include <iostream>
 #include "Player.h"
 
+using namespace std;
+
 Player::Player()
 {
+    horizontal = 0;
+    vertical = 0;
 
+}
+
+void Player::CallInput()
+{
+    char userInput = ' ';
+    cin >> userInput;
+
+    switch (userInput)
+    {
+    case 'w':
+        vertical++;
+        cout << vertical << endl;
+        break;
+
+    case 's':
+        vertical--;
+        cout << vertical << endl;
+        break;
+
+    case 'a':
+        horizontal--;
+        cout << horizontal << endl;
+        break;
+
+    case 'd':
+        horizontal++;
+        cout << horizontal << endl;
+        break;
+
+    case 'e':
+        isGameOver = true;
+        break;
+    
+    default:
+        break;
+    }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Treasure-Hunt
-Game developed in C++ using the command line
